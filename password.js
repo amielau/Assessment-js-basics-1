6810396f88cd60057e85940d2d2db454293b4a18
amielau <amielperkins@gmail.com>
2022-02-14 09:55:13 -0700

    password validator after test problem solving

--- password.js
+++ password.js
@@ -4,6 +4,19 @@ const reader = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
+reader.question("Type in a password", function(input) {
+    if(input.length > 10) {
+        console.log("your password is sufficient");
+    } else {
+        console.log("not a valid password");
+    }
+});
+
+/*
+
+this is my original code below -
+
 let greeting = "Welcome to the password validator tool"
 console.log(greeting);
 
@@ -11,7 +24,7 @@ reader.question("enter your password here", function(answer) {
     console.log("my password is" + answer);
 });
 
-var password = window.answer
+var password = answer
 console.log(password);
 
 if (password.length >= 10) {
@@ -19,4 +32,4 @@ if (password.length >= 10) {
  } else {
      console.log('invalid password. password must be at least 10 characters')
  }
-
+*/
